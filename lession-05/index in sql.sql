daaec6d88dece492f5c24c948665168fac14ce1a
unknown <shuhari0123@gmail.com>
2023-12-01 13:57:00 +0700

    thuchanh

--- lession-05/index in sql.sql
+++ lession-05/index in sql.sql
@@ -211,3 +211,20 @@ call setCounter(@counter, 1);
 call setCounter(@counter, 1);
 call setCounter(@counter, 5);
 select @counter;
+
+create view customer_views as 
+select customerNumber, customerName, phone 
+from customers;
+
+select * from customer_views;
+
+create or replace view view_name as 
+select customerNumber, customerName, contactFirstName, contactLastName, phone
+from customers
+where city = 'Nantes';
+
+select * from view_name;
+
+drop view view_name;
+
+drop view customer_views;
