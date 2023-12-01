ad9ab6ecaf4d1fdef7e7aeb831d3a42fa8462a00
unknown <shuhari0123@gmail.com>
2023-12-01 11:27:26 +0700

    thuchanh

--- lession-05/index in sql.sql
+++ lession-05/index in sql.sql
@@ -169,3 +169,45 @@ create procedure findAllCustomer()
 begin 
 select * from customers where customerNumber = 175;
 end //
+
+delimiter // 
+create procedure getCushById
+(IN cusNum INT(11))
+BEGIN
+select * from customers where customerNumber = cusNum;
+end //
+delimiter ;
+
+call getCushById(175);
+
+delimiter // 
+create procedure getCustomersCountByCity(
+IN in_city varchar(50),
+OUT total INT
+)
+BEGIN
+select count (customerNumber)
+INTO total 
+from customers
+where city = in_city;
+END //
+delimiter ;
+
+call getCustomersCountByCity('Lyon', @total);
+select @total;
+
+delimiter //
+create procedure setCounter(
+INOUT counter int,
+IN inc INT
+)
+BEGIN
+set counter = counter + inc;
+end //
+delimiter ;
+
+set @counter = 1;
+call setCounter(@counter, 1);
+call setCounter(@counter, 1);
+call setCounter(@counter, 5);
+select @counter;
